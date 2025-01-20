74dcb738d9f8fa5d78672070950f2f203d4adf32
e1shiy <1eshiy@vk.com>
2025-01-20 15:38:58 +0700

    sending emails from feedback page has been added

--- pages/feedback/app.js
+++ pages/feedback/app.js
@@ -27,3 +27,99 @@ function setLanguage(choise) {
     document.getElementById('aboutUsButton').querySelector('p').textContent = language.aboutUs
     document.getElementById('feedbackButton').querySelector('p').textContent = language.feedback
 } // доделать
+
+
+
+const form = document.querySelector('.feedback__form')
+const nameInput = document.getElementById('inputName')
+const emailInput = document.getElementById('inputEmail')
+const messageInput = document.getElementById('messageTextArea')
+const formSubmitButton = form.querySelector('button[type="submit"]')
+const formOverlay = document.querySelector('.form-overlay')
+const formOverlaySending = formOverlay.querySelector('.form-overlay__sending')
+const formOverlaySent = formOverlay.querySelector('.form-overlay__sent')
+const formOverlayMessage = formOverlay.querySelector('.form-overlay__sent-result')
+const formOverlayButton = formOverlay.querySelector('.form-overlay__sent-button')
+
+const formValidate = (name, email, message) => {
+    const isNameValid = name => {
+        const syntaxMatch = Boolean(name.match(/^[a-zA-Zа-яА-Я\s-]+$/i))
+        const lengthMatch = name.length < 300
+        return syntaxMatch && lengthMatch
+    }    
+    const isEmailValid = email => {
+        const syntaxMatch = Boolean(email.match(/^[a-zA-Z0-9]([a-zA-Z\.0-9-_]*[a-zA-Z0-9])?@[a-zA-Z]+\.[a-zA-Z]{2,}$/i))
+        const lengthMatch = email.length < 300
+        return syntaxMatch && lengthMatch
+    }
+    const isMessageValid = message => {
+        return message.length < 1000 && message.length > 0
+    }
+    
+    if (isNameValid(name)) {
+        nameInput.classList.remove('feedback__form-input--failed')
+        nameInput.classList.add('feedback__form-input--succeed')
+    } else {
+        nameInput.classList.remove('feedback__form-input--succeed')
+        nameInput.classList.add('feedback__form-input--failed')
+    }
+
+    if (isEmailValid(email)) {
+        emailInput.classList.add('feedback__form-input--failed')
+        emailInput.classList.add('feedback__form-input--succeed')
+    } else {
+        emailInput.classList.remove('feedback__form-input--succeed')
+        emailInput.classList.add('feedback__form-input--failed')
+    }
+
+    if (isMessageValid(message)) {
+        messageInput.classList.remove('feedback__form-message--failed')
+        messageInput.classList.add('feedback__form-message--succeed')
+    } else {
+        messageInput.classList.remove('feedback__form-message--succeed')
+        messageInput.classList.add('feedback__form-message--failed')
+    }
+
+    return isNameValid(name) && isEmailValid(email) && isMessageValid(message)
+}
+
+async function sendForm() {
+    const name = nameInput.value
+    const email = emailInput.value
+    const message = messageInput.value
+    
+    const isFormValid = formValidate(name, email, message)
+    if (!isFormValid) { return }
+    
+    const formData = new FormData(form)
+    formOverlay.showModal()
+
+    const response = await fetch('sendmail.php', {
+        method: 'POST',
+        body: formData
+    })
+
+    const result =  await response.json()
+    
+    formOverlaySending.classList.add('visually-hidden')
+    formOverlaySent.classList.remove('visually-hidden')
+    formOverlayMessage.textContent = result.message
+    
+    form.reset()
+    nameInput.classList.remove('feedback__form-input--succeed')
+    nameInput.classList.remove('feedback__form-input--failed')
+    emailInput.classList.remove('feedback__form-input--succeed')
+    emailInput.classList.remove('feedback__form-input--failed')
+    messageInput.classList.remove('feedback__form-message--succeed')
+    messageInput.classList.remove('feedback__form-message--failed')
+}
+
+formSubmitButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    sendForm()
+})
+
+formOverlayButton.addEventListener('click', () => {
+    formOverlaySending.classList.remove('visually-hidden')
+    formOverlaySent.classList.add('visually-hidden')
+})
---
+++ pages/feedback/sendmail.php
@@ -0,0 +1,41 @@
+<?php
+require __DIR__ . '/../../vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$response = [];
+try {
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
+    $mail->setLanguage('ru', __DIR__ . '/../../vendor/phpmailer/phpmailer/language/');
+    $mail->isHTML(true);
+
+    $mail->isSMTP();
+    $mail->Host = 'smtp.mail.ru';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'veretnov.aleksey@mail.ru';
+    $mail->Password = 'jATNekBPR69AQkRED23n';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port = 465;
+
+    $mail->setFrom('veretnov.aleksey@mail.ru');
+    $mail->addAddress('1eshiy@vk.com');
+    $mail->Subject = 'Форма обратной связи (KrasGid)';
+
+    $body = '<p><strong>Имя: </strong>' . $_POST['firstName'] . '</p>';
+    $body .= '<p><strong>E-mail: </strong>' . $_POST['email'] . '</p>';
+    $body .= '<p><strong>Сообщение: </strong>' . $_POST['message'] . '</p>';
+
+    $mail->Body = $body;
+    $mail->send();
+
+    $response['status'] = 'success';
+    $response['message'] = 'сообщение успешно отправлено!';
+} catch (Exception $e) {
+    $response['status'] = 'error: ' . __DIR__ . '../../../vendor';
+    $response['message'] = 'не удалось отправить сообщение :(';
+}
+
+header('Content-Type: application/json');
+echo json_encode($response);
